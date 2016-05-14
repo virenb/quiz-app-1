0c4519b646f5528b1f4ea3664b0c700c0c5f8f01
Chie <akiba.chie@gmail.com>
2016-05-13 19:29:33 -0700

    Added next question button and play again button

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	//hide the feedback part until they click the submit button
+	//enable submit button to work
+	//if else statement for the quiz
+		//show feedback to the user
+			//if correct answer, show "You got it right!!!"
+			//if wrong answer, show "Wrong!!!" and "The correct answer is..."
+			//show the trivia information at the bottom for both correct and wrong scenario
+	//once user answers the question, enable the next question button
+		//once the user clicks the next question button, make "questions left" go down one number
+		//the player score should go up one number +1
+});
