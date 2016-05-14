cc0676ae0f5b9dceb8784af931c58b3d318e2a72
Chie <akiba.chie@gmail.com>
2016-05-14 13:17:24 -0700

    Added global variables and question arrays. Changed the class names in index.html to better fit app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//global variables
 var quizQuestions = $('.options');
 var questionsLeft = $('.questions-left');
 var playerScore = $('#count');
@@ -7,9 +8,53 @@ var playerFeedback = $('.feedback');
 var answer = $('.correct-answer');
 var triviaInfo = $('.trivia-info');
 
+//question array
+var questions = [{
+	question: "Which Seven Wonders of the Ancient World was built first?",
+	choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
+	questionNumber: 0,
+	correct: 0,
+	triviaInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+	},
+	{
+	question: "Which Seven Wonders of the Ancient World was built first?",
+	choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
+	questionNumber: 1,
+	correct: 1,
+	triviaInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+},
+{
+	question: "Which Seven Wonders of the Ancient World was built first?",
+	choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
+	questionNumber: 2,
+	correct: 3,
+	triviaInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+},
+{
+	question: "Which Seven Wonders of the Ancient World was built first?",
+	choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
+	questionNumber: 3,
+	correct: 2,
+	triviaInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+},
+{
+	question: "Which Seven Wonders of the Ancient World was built first?",
+	choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
+	questionNumber: 4,
+	correct: 1,
+	triviaInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+}]
+
+
 $(document).ready(function() {
-	//hide the feedback part until they click the submit button
+	//hide everything but the question part until they click the submit button
+	ending.hide();
+	playerFeedback.hide();
+	triviaInfo.hide();
 	//enable submit button to work
+	$('.submit-button').click(function() {
+
+	});
 	//if else statement for the quiz
 		//show feedback to the user
 			//if correct answer, show "You got it right!!!"
