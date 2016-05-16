94537087189593978ec44d801952f3dbdda9b63d
Chie <akiba.chie@gmail.com>
2016-05-15 23:30:12 -0700

    Enabled the trivia information to show when the user submits their answer

--- js/app.js
+++ js/app.js
@@ -3,33 +3,38 @@ var quiz = [{
     question: "Which Seven Wonders of the Ancient World was built first?",
     choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
     correct: 0,
+    answer: "Hanging Gardens of Babylon",
     trivia: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
 }, {
     question: "Who was the first emperor of China?",
     choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
     correct: 1,
+    answer: "Great Pyramid of Giza",
     trivia: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
 }, {
     question: "Where was the first law made in?",
     choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
     correct: 2,
+    answer: "Rhodes",
     trivia: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
 }, {
     question: "Who won the war of whatever?",
     choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
     correct: 3,
+    answer: "Colossus of Rhodes",
     trivia: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
 }, {
     question: "Which nation arose due to the Cold War?",
     choices: ["Hanging Gardens of Babylon", "Great Pyramid of Giza", "Rhodes", "Colossus of Rhodes", "Lighthouse of Alexandria"],
     correct: 4,
+    answer: "Lighthouse of Alexandria",
     trivia: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
 }];
 
 //global variables
 var ending = $('.end-page');
 var nextQuestion = $('.next-question-button');
-var triviaInfo = $('.trivia-info');
+var triviaInfo = $('#trivia-info').hide();
 
 var playerAnswer;
 var correctAnswer;
@@ -41,7 +46,7 @@ $(document).ready(function() {
     //hide everything but the question part until they click the submit button
     ending.hide();
     nextQuestion.hide();
-    triviaInfo.hide();
+    triviaInfo;
     //function for accessing the objects and properties in the quizQuestions array
     var loadQuestion = function() {
             newQuestion = "<h3>" + quiz[currentQuestion].question + "</h3>";
@@ -59,6 +64,9 @@ $(document).ready(function() {
         } else {
             wrongAnswer = $('.feedback').append("<h3>Sorry, you got it wrong.</h3>");
         }
+        $('.feedback').append("<h4>The correct answer is:</br><strong> " + quiz[currentQuestion].answer + "</strong></h4>");
+        triviaInfo.show();
+        $('#trivia-info').append("<p>" + quiz[currentQuestion].trivia + "</p>");
         nextQuestion.show();
     })
     //show next question when Next Question button is clicked
@@ -66,7 +74,11 @@ $(document).ready(function() {
         currentQuestion++;
         $('.main-quiz').empty();
         $('.feedback').empty();
+        $('.trivia-info').empty();
         nextQuestion.hide();
         loadQuestion();
     })
+    if (currentQuestion == 5) {
+        ending.show();
+    }
 });
